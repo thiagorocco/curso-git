d1f49786c75e272e53d5835b2da5d0e5b84a3ab0
thiagorocco <deroccotr@gmail.com>
2023-04-23 18:29:30 -0300

    Adicionando código randomNumbers em Python

---
+++ randomNumbers.py
@@ -0,0 +1,19 @@
+import random
+
+n = random.randint(1,10)
+tentativas = 0
+myNumber = 0
+acertar = False
+while acertar==False:
+    tentativas+=1
+    myNumber = int(input('Tente adivinhar um número entre 1 e 10: '))
+    if myNumber == n:
+        if tentativas == 1:
+            print('Parabéns você acertou na primeira tentativa!!!')
+        else:
+            print('Você acertou em ',tentativas,' tentativas')
+        acertar = True
+    elif myNumber > n:
+        print('O número sorteado é menor que ',myNumber)
+    else:
+        print('O número sorteado é maior que ',myNumber)
